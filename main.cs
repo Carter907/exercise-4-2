07f21a968bc75f1bb5f8f9105f49ac09f12f3a34
Carter <102479896+Carter907@users.noreply.github.com>
2024-06-14 15:58:17 +0000

    added base code impl

--- main.cs
+++ main.cs
@@ -2,6 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    var input = Console.ReadLine();
+    while (input != "Z") {
+      
+    }
   }
 }
