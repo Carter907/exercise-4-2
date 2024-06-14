066a932f0c7b5223881f22ea874c397654a9d575
Carter <102479896+Carter907@users.noreply.github.com>
2024-06-14 16:01:22 +0000

    Added some more base code

--- main.cs
+++ main.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+    Console.WriteLine("salesperson:");
     var input = Console.ReadLine();
     while (input != "Z") {
       
